a1380140f85d6df08305b994882fcbfd363873b0
James Billings <james_billings@me.com>
2017-06-29 15:30:31 -0600

    remove

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Comedy Resistance')
+  res.send('Comedy Resistance Yea!')
 })
 
 app.listen(3000, function () {
