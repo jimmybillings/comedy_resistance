39988bf48d8d9c9eb9d74f78f5a62e1fac5b88ac
James Billings <james_billings@me.com>
2017-07-05 21:44:26 -0600

    add auth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('http-auth');
+
+var basic = auth.basic({
+  realm: "Web."
+}, function (username, password, callback) { // Custom authentication method.
+  callback(username === "swing" && password === "left");
+}
+);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', auth.connect(basic), function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
